6a2b677a74ca2311681574d071f381c944158d4f
Symplexia Labs <suporte@symplexia.com.br>
2023-12-01 16:35:55 -0300

    Add Icons Plexchain

--- packages/ui-plex/src/components/Image/tokens.ts
+++ packages/ui-plex/src/components/Image/tokens.ts
@@ -7,6 +7,27 @@ const tokens = {
     address: {
       56: "0x32d9F70F6eF86718A51021ad269522Abf4CFFE49",
       97: "0xA2af2640A694f91632e60befc7Fc30C1b787D505",
+      1149: "0x7589CF3115E060673FD0109fA6891464E13947A1"
+    },
+    decimals: 18,
+    projectLink: "https://swap.plexfinance.us/",
+  },
+  wplex: {
+    symbol: "WPLEX",
+    address: {
+      56: "",
+      97: "",
+      1149: "0x50245424Afc53E67Ca1AAD2C90401568C0eFf53A"
+    },
+    decimals: 18,
+    projectLink: "https://swap.plexfinance.us/",
+  },
+  usdp: {
+    symbol: "usdp",
+    address: {
+      56: "",
+      97: "",
+      1149: "0x69D1D347088e8916aC107b6679927C4EC5faB4a0"
     },
     decimals: 18,
     projectLink: "https://swap.plexfinance.us/",
@@ -16,6 +37,7 @@ const tokens = {
     address: {
       56: "0x23396cf899ca06c4472205fc903bdb4de249d6fc",
       97: "",
+      1149: "",
     },
     decimals: 18,
     projectLink: "https://mirror.finance/",
@@ -25,6 +47,7 @@ const tokens = {
     address: {
       56: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
       97: "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",
+      1149: "",
     },
     decimals: 18,
     projectLink: "https://swap.plexfinance.us/",
@@ -33,7 +56,8 @@ const tokens = {
     symbol: "BUSD",
     address: {
       56: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
-      97: "",
+      97: "0x8516Fc284AEEaa0374E66037BD2309349FF728eA",
+      1149: "",
     },
     decimals: 18,
     projectLink: "https://www.paxos.com/busd/",
@@ -43,6 +67,7 @@ const tokens = {
     address: {
       56: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
       97: "",
+      1149: "",
     },
     decimals: 18,
     projectLink: "https://ethereum.org/en/",
@@ -52,6 +77,7 @@ const tokens = {
     address: {
       56: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
       97: "",
+      1149: "",
     },
     decimals: 18,
     projectLink: "https://www.centre.io/usdc",
@@ -61,6 +87,7 @@ const tokens = {
     address: {
       56: "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3",
       97: "",
+      1149: "",
     },
     decimals: 18,
     projectLink: "https://www.makerdao.com/",
@@ -70,6 +97,7 @@ const tokens = {
     address: {
       56: "0x55d398326f99059fF775485246999027B3197955",
       97: "0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5",
+      1149: "",
     },
     decimals: 18,
     projectLink: "https://tether.to/",
@@ -79,6 +107,7 @@ const tokens = {
     address: {
       56: "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
       97: "0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5",
+      1149: "",
     },
     decimals: 18,
     projectLink: "https://bitcoin.org/",
@@ -88,6 +117,7 @@ const tokens = {
     address: {
       56: "0xbA2aE424d960c26247Dd6c32edC70B295c744C43",
       97: "",
+      1149: "",
     },
     decimals: 8,
     projectLink: "https://dogecoin.com/",
